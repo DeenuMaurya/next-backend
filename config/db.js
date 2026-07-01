7402244e990b06e551ae2627866b4e1e116c2fec
anubhav2706 <114422081+anubhav2706@users.noreply.github.com>
2026-07-01 13:06:44 +0530

    package changed

--- config/db.js
+++ config/db.js
@@ -6,8 +6,17 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI, {
             tls: true,
             tlsCAFile: path.join(__dirname, "../global-bundle.pem"),
-        });
 
+            maxPoolSize: 200,
+            minPoolSize: 20,
+
+            maxIdleTimeMS: 30000,
+            socketTimeoutMS: 120000,
+            connectTimeoutMS: 30000,
+            serverSelectionTimeoutMS: 30000,
+
+            retryWrites: false
+        });
         console.log("DocumentDB Connected");
     } catch (err) {
         console.error("Connection Error:", err);
